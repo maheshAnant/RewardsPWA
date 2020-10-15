bb56c9de21b6f68b09df6c13764c01f3ac6f7f4a
maheshAnant <72908016+maheshAnant@users.noreply.github.com>
2020-10-15 13:40:07 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 'dsn' => '',
-'hostname' => 'localhost',
+'hostname' => '144.76.139.247',
 'username' => 'rewardsPWA',
 'password' => 'ghdftnjsjkniudwv2',
 'database' => 'rewardsPWA',
